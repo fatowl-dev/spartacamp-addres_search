481d5e66dfc4d497706ca624ec43d8b1018f6681
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-09 13:46:52 +0900

    add: 整形して表示する機能を追加

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import requests
-zipcode = input('郵便番号?: ')
+
+
+# データを加工して表示する
+def display_address(dic: dict):
+    if dic['message'] is not None:
+        print(dic['message'])
+        return
+
+    for address_dic in dic['results']:
+        message = address_dic["address1"]
+        message += address_dic["address2"]
+        message += address_dic["address3"]
+        print(message)
+
+
+# zipcode = input('郵便番号?: ')
+zipcode = '0287302'
 if len(zipcode) != 7 or not zipcode.isdigit():
     print('エラー: 7桁の数字を入力してください')
     exit()
 
-
 response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
 print(response)
 print(response.text)
+display_address(response.json())
