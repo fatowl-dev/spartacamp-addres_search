ddd7ab023d7eb19998a48910878d47777c42ef0f
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-09 13:59:56 +0900

    fix: 不正な郵便番号のときにエラー表示

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import requests
 # データを加工して表示する
 def display_address(dic: dict):
     if dic['message'] is not None:
-        print(dic['message'])
+        print('エラー:', dic['message'])
+        return
+
+    if dic['results'] is None:
+        print('エラー: 存在しない郵便番号です')
         return
 
     for address_dic in dic['results']:
@@ -14,13 +18,14 @@ def display_address(dic: dict):
         print(message)
 
 
-# zipcode = input('郵便番号?: ')
-zipcode = '0287302'
+zipcode = input('郵便番号?: ')
+# zipcode = '0287302'
 if len(zipcode) != 7 or not zipcode.isdigit():
     print('エラー: 7桁の数字を入力してください')
     exit()
 
 response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
-print(response)
-print(response.text)
-display_address(response.json())
+# print(response)
+# print(response.text)
+res_dic = response.json()
+display_address(res_dic)
