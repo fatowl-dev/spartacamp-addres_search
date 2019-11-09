f479ba1add24449bb3162eaee6109ad272309911
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-09 13:26:10 +0900

    add: 郵便番号の入力処理

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import requests
+zipcode = input('郵便番号?: ')
+if len(zipcode) != 7 or not zipcode.isdigit():
+    print('エラー: 7桁の数字を入力してください')
+    exit()
 
-response = requests.get('http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287302')
+
+response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
 print(response)
 print(response.text)
