25e948770bd23fc901c99a4082a38a5684852099
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-09 15:10:08 +0900

    add: search_address関数

--- app.py
+++ app.py
@@ -1,31 +1,41 @@
 import requests
 
 
-# データを加工して表示する
-def display_address(dic: dict):
-    if dic['message'] is not None:
-        print('エラー:', dic['message'])
-        return
+# 郵便番号を入力すると住所のリストを返す
+# エラーの場合は空のリストを返す
+def search_address(zipcode: str) -> list:
+    response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
+    res_dic = response.json()
+    if res_dic['message'] is not None:
+        print('エラー:', res_dic['message'])
+        return []
 
-    if dic['results'] is None:
+    if res_dic['results'] is None:
         print('エラー: 存在しない郵便番号です')
-        return
+        return []
 
-    for address_dic in dic['results']:
-        res_text = address_dic["address1"]
-        res_text += address_dic["address2"]
-        res_text += address_dic["address3"]
-        print(res_text)
+    address_list = []
+    for address_dic in res_dic['results']:
+        text = address_dic["address1"]
+        text += address_dic["address2"]
+        text += address_dic["address3"]
+        address_list.append(text)
+
+    return address_list
 
 
-zipcode = input('郵便番号?: ')
-# zipcode = '0287302'
-if len(zipcode) != 7 or not zipcode.isdigit():
-    print('エラー: 7桁の数字を入力してください')
-    exit()
+def main():
+    zipcode = input('郵便番号?: ')
+    # zipcode = '0287302'
 
-response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
-# print(response)
-# print(response.text)
-res_dic = response.json()
-display_address(res_dic)
+    if len(zipcode) != 7 or not zipcode.isdigit():
+        print('エラー: 7桁の数字を入力してください')
+        exit()
+
+    address_list = search_address(zipcode)
+    for address in address_list:
+        print(address)
+
+
+if __name__ == '__main__':
+    main()
